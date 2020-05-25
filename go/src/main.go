7a8e29cff9e50ea1cf806d2a76715cff2e411b5c
doogz <wesley.dugan@verizonmedia.com>
2020-05-24 22:04:07 -0400

    adding

--- go/src/main.go
+++ go/src/main.go
@@ -2,10 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"parser"
 )
 
 func main() {
-	fmt.Println("Init")
-	parser.ParseLogDir("/Users/wdugan/Documents/PersonalGit/log_ripper/log_files_test")
+	// "/Users/wdugan/Documents/PersonalGit/log_ripper/log_files_test"
+	pathToLogs := os.Args[1]
+	testPath, err := os.Stat(pathToLogs)
+
+	if testPath.IsDir() {
+		parser.ParseLogDir(pathToLogs)
+	} else {
+		parser.ParseLogFile(pathToLogs)
+	}
+	if err != nil {
+		fmt.Println(err)
+	}
 }
