9c79d897ac4eecf28b1634077d70e071cd0648e5
doogz <wesley.dugan@verizonmedia.com>
2020-05-25 11:52:49 -0400

    works

--- go/src/main.go
+++ go/src/main.go
@@ -11,17 +11,21 @@ func main() {
 	// "/Users/wdugan/Documents/PersonalGit/log_ripper/log_files_test"
 	var numberOfErrors int
 	pathToLogs := os.Args[1]
+	databaseName := os.Args[2]
+	databaseUser := os.Args[3]
+	databasePass := os.Args[4]
 	testPath, err := os.Stat(pathToLogs)
 
 	if testPath.IsDir() {
 		numberOfErrors = parser.ParseLogDir(pathToLogs)
 		fmt.Println(numberOfErrors)
+		poster.Poster("http://192.168.1.10:8086", databaseName, databaseUser, databasePass)
 	} else {
 		numberOfErrors = parser.ParseLogFile(pathToLogs)
 		fmt.Println(numberOfErrors)
+		poster.Poster("http://192.168.1.10:8086", databaseName, databaseUser, databasePass)
 	}
 	if err != nil {
 		fmt.Println(err)
 	}
-	poster.Poster("https://httpbin.org/post")
 }
--- go/src/poster/poster.go
+++ go/src/poster/poster.go
@@ -2,16 +2,34 @@ package poster
 
 import (
 	"fmt"
+	"io/ioutil"
+	"log"
 	"net/http"
+	"strconv"
+	"strings"
+	"time"
 )
 
 //Poster posts
-func Poster(url string) {
-	r, err := http.Post(url, "hello", nil)
-	r.Header.Add("Authorization", "Token username:password")
-	r.Header.Add("content-type", "application/x-www-form-urlencoded")
+func Poster(url string, databaseName string, databaseUser string, databasePass string) {
+	var client = &http.Client{}
+	var currentTime = strconv.FormatInt(time.Now().Unix(), 10)
+	var data = strings.NewReader(`cpu_load_short,host=server01,region=us-west value=0.64` + currentTime + `000000000`)
+
+	r, err := http.NewRequest("POST", url+"/write?db="+databaseName, data)
+	r.Header.Set("Authorization", "Token "+databaseUser+":"+databasePass)
+
 	if err != nil {
 		fmt.Println(err)
 	}
-	fmt.Println(r)
+	resp, err := client.Do(r)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	bodyText, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%s\n", bodyText)
 }
